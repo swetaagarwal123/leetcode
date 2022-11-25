c3afc31181c8c95acbedb468fb4f4a90ce105ad4
swetaagarwal123 <111454097+swetaagarwal123@users.noreply.github.com>
2022-11-25 19:37:23 +0530

    Create 58.java

---
+++ 58.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int i = 0;
+        int flag = 1;
+        s=s.trim();
+
+        for (i= s.lastIndexOf(" ")+1; i<s.length()-1;i++) {
+            flag++;
+        }
+        return flag;
+        
+    }
+}
